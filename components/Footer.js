d0fa9dccb60d932c451498a69100b1ca64df25d8
Ehfaz Mahee <mahee504@gmail.com>
2024-02-16 20:56:45 +0600

    3rd commit: added logos in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,94 +1,98 @@
 import React from 'react';
 
 const Footer = () => {
-  return (
-    <footer className="text-white text-center text-lg-start bg-dark">
-      {/* Grid container */}
-      <div className="container p-4">
-        {/*Grid row*/}
-        <div className="row mt-4">
-          {/*Grid column*/}
-          <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
-            <h5 className="text-uppercase mb-4">About company</h5>
-            <p>
-              At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-              voluptatum deleniti atque corrupti.
-            </p>
-            <p>
-              Blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas
-              molestias.
-            </p>
-            <div className="mt-4">
-              {/* Social media icons */}
-              <a href="#" className="btn btn-floating btn-warning btn-lg"><i className="fab fa-facebook-f"></i></a>
-              <a href="#" className="btn btn-floating btn-warning btn-lg"><i className="fab fa-dribbble"></i></a>
-              <a href="#" className="btn btn-floating btn-warning btn-lg"><i className="fab fa-twitter"></i></a>
-              <a href="#" className="btn btn-floating btn-warning btn-lg"><i className="fab fa-google-plus-g"></i></a>
+    return (
+        <footer className="text-white text-center text-lg-start bg-dark">
+            {/* Grid container */}
+            <div className="container p-4">
+                {/*Grid row*/}
+                <div className="row mt-4">
+                    {/*Grid column*/}
+                    <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
+                        <h5 className="text-uppercase mb-4">About company</h5>
+                        <p>
+                            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+                            voluptatum deleniti atque corrupti.
+                        </p>
+                        <p>
+                            Blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas
+                            molestias.
+                        </p>
+                        <div className="mt-4">
+                            {/* Social media icons */}
+
+                            <a href="#" className="btn btn-floating btn-warning btn-lg mx-2">
+                                <ion-icon name="logo-facebook"></ion-icon>
+                            </a>
+                            <a href="#" className="btn btn-floating btn-warning btn-lg mx-2"><ion-icon name="logo-youtube"></ion-icon></a>
+                            <a href="#" className="btn btn-floating btn-warning btn-lg mx-2"><ion-icon name="logo-twitter"></ion-icon></a>
+                            <a href="#" className="btn btn-floating btn-warning btn-lg mx-2"><ion-icon name="logo-instagram"></ion-icon></a>
+                            <a href="#" className="btn btn-floating btn-warning btn-lg"><ion-icon name="logo-whatsapp"></ion-icon></a>
+                        </div>
+                    </div>
+                    {/*Grid column*/}
+
+                    {/*Grid column*/}
+                    <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+                        <h5 className="text-uppercase mb-4 pb-1">Search something</h5>
+                        {/* Search form with Bootstrap classes */}
+                        <div className="input-group mb-4">
+                            <input type="text" id="formControlLg" className="form-control form-control-lg" placeholder="Search" aria-label="Search" aria-describedby="button-addon2" />
+                            <button className="btn btn-outline-warning" type="button" id="button-addon2">Search</button>
+                        </div>
+                        {/* Contact information */}
+                        <ul className="fa-ul" style={{ marginLeft: '1.65em' }}>
+                            <li className="mb-3">
+                                <span className="fa-li"><i className="fas fa-home"></i></span><span className="ms-2">New York, NY 10012, US</span>
+                            </li>
+                            <li className="mb-3">
+                                <span className="fa-li"><i className="fas fa-envelope"></i></span><span className="ms-2">info@example.com</span>
+                            </li>
+                            <li className="mb-3">
+                                <span className="fa-li"><i className="fas fa-phone"></i></span><span className="ms-2">+ 01 234 567 88</span>
+                            </li>
+                            <li className="mb-3">
+                                <span className="fa-li"><i className="fas fa-print"></i></span><span className="ms-2">+ 01 234 567 89</span>
+                            </li>
+                        </ul>
+                    </div>
+                    {/*Grid column*/}
+
+                    {/*Grid column*/}
+                    <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+                        <h5 className="text-uppercase mb-4">Opening hours</h5>
+                        {/* Opening hours table */}
+                        <table className="table text-center text-white">
+                            <tbody className="font-weight-normal">
+                                <tr>
+                                    <td>Mon - Thu:</td>
+                                    <td>8am - 9pm</td>
+                                </tr>
+                                <tr>
+                                    <td>Fri - Sat:</td>
+                                    <td>8am - 1am</td>
+                                </tr>
+                                <tr>
+                                    <td>Sunday:</td>
+                                    <td>9am - 10pm</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    {/*Grid column*/}
+                </div>
+                {/*Grid row*/}
             </div>
-          </div>
-          {/*Grid column*/}
+            {/* Grid container */}
 
-          {/*Grid column*/}
-          <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
-            <h5 className="text-uppercase mb-4 pb-1">Search something</h5>
-            {/* Search form with Bootstrap classes */}
-            <div className="input-group mb-4">
-              <input type="text" id="formControlLg" className="form-control form-control-lg" placeholder="Search" aria-label="Search" aria-describedby="button-addon2" />
-              <button className="btn btn-outline-warning" type="button" id="button-addon2">Search</button>
+            {/* Copyright */}
+            <div className="text-center p-3 bg-secondary">
+                © 2020 Copyright:
+                <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
             </div>
-            {/* Contact information */}
-            <ul className="fa-ul" style={{ marginLeft: '1.65em' }}>
-              <li className="mb-3">
-                <span className="fa-li"><i className="fas fa-home"></i></span><span className="ms-2">New York, NY 10012, US</span>
-              </li>
-              <li className="mb-3">
-                <span className="fa-li"><i className="fas fa-envelope"></i></span><span className="ms-2">info@example.com</span>
-              </li>
-              <li className="mb-3">
-                <span className="fa-li"><i className="fas fa-phone"></i></span><span className="ms-2">+ 01 234 567 88</span>
-              </li>
-              <li className="mb-3">
-                <span className="fa-li"><i className="fas fa-print"></i></span><span className="ms-2">+ 01 234 567 89</span>
-              </li>
-            </ul>
-          </div>
-          {/*Grid column*/}
-
-          {/*Grid column*/}
-          <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
-            <h5 className="text-uppercase mb-4">Opening hours</h5>
-            {/* Opening hours table */}
-            <table className="table text-center text-white">
-              <tbody className="font-weight-normal">
-                <tr>
-                  <td>Mon - Thu:</td>
-                  <td>8am - 9pm</td>
-                </tr>
-                <tr>
-                  <td>Fri - Sat:</td>
-                  <td>8am - 1am</td>
-                </tr>
-                <tr>
-                  <td>Sunday:</td>
-                  <td>9am - 10pm</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          {/*Grid column*/}
-        </div>
-        {/*Grid row*/}
-      </div>
-      {/* Grid container */}
-
-      {/* Copyright */}
-      <div className="text-center p-3 bg-secondary">
-        © 2020 Copyright:
-        <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-      </div>
-      {/* Copyright */}
-    </footer>
-  );
+            {/* Copyright */}
+        </footer>
+    );
 };
 
 export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,8 @@ function App({ Component, pageProps }) {
     <Head>
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" href="myProjects/webProject/icofont/css/icofont.min.css"/>
+
       <title>Mara Blogs</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-+nBsK5eMK7xejpBkHxBz4O8Oj/U4W15qEmjDe/+nKRPI7tA3C7TlgxsixWXY+Nnx2W9VYNRki10TkOMgaGU3iA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -16,6 +18,8 @@ function App({ Component, pageProps }) {
     </Head>
     <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></Script>
     <Script src="https://use.fontawesome.com/a4e2acc8ae.js"></Script>
+    <Script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></Script>
+<Script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></Script>
     <Navbar />
     <Component {...pageProps} />
     <Footer/>
